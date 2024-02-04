41910faa3c44a243ce7e8a652c97dcccbc960af1
Prateek Agrawal <138208959+Prateek0305@users.noreply.github.com>
2024-02-05 00:24:43 +0530

    Update main.py

--- Day 02/main.py
+++ Day 02/main.py
@@ -4,5 +4,5 @@ people=int(input("How many people to split the bill?"))
 split=bill/people
 tip=int(input("What percent tip would you like to give? 10 12 or 15?"))
 final=float(split+split*(tip/100))
-final1="{:.2f}".format(final=float(split+split*(tip/100)))
+final1="{:.2f}".format(final)
 print("you have to pay "+str(final1)+" each")
