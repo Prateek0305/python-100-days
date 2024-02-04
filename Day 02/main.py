594fb395e4d9dfb1124f1a2c3b3a90c5fd6c50d5
Prateek Agrawal <138208959+Prateek0305@users.noreply.github.com>
2024-02-05 00:15:07 +0530

    Update main.py

--- Day 02/main.py
+++ Day 02/main.py
@@ -1,8 +1,8 @@
 print("Welcome to tip calulator")
 bill=int(input("What was the total bill?"))
 people=int(input("How many people to split the bill?"))
-split=round(bill/people,2)
+split=bill/people
 tip=int(input("What percent tip would you like to give? 10 12 or 15?"))
 final=float(split+split*(tip/100))
-final1="{:.2f}".format(final)
+final1="{:.2f}".format(final=float(split+split*(tip/100)))
 print("you have to pay "+str(final1)+" each")
