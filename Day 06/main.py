1cb2f8d14160c355bbf94315b4545ecf59f7d451
Prateek Agrawal <138208959+Prateek0305@users.noreply.github.com>
2025-03-14 18:37:18 +0530

    Update main.py

--- Day 06/main.py
+++ Day 06/main.py
@@ -1,1 +1,74 @@
+#Hurdle 1 and hurdle 2
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+    
+while not at_goal():
+    move();
+    turn_left();
+    move();
+    turn_right();
+    move();
+    turn_right();
+    move();
+    turn_left();
+  
+#Hurdle 3
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+while not at_goal():
+    if front_is_clear():
+        move()
+    else :
+        turn_left()
+        move()
+        turn_right()
+        move()
+        turn_right()
+        move()
+        turn_left()
 
+#Hurdle 4
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+def jump():
+    turn_left()
+    while wall_on_right(): 
+        move()
+    turn_right()
+    move() 
+    turn_right()
+    while front_is_clear(): 
+        move()
+    turn_left()
+
+while not at_goal():
+    if front_is_clear():  
+        move()  
+    else:
+        jump()  
+        
+    
+#Maze
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+def maze_move():
+    if right_is_clear():
+        turn_right()
+        move()
+    elif front_is_clear():
+        move()
+    else:
+        turn_left()
+
+while not at_goal():
+    maze_move()
